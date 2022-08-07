273d0199633c751c2e32c993d54d6a77b80624fb
Niharika <110163537+niharikaasinghal@users.noreply.github.com>
2022-08-07 18:10:02 +0530

    Add files via upload

---
+++ taskapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path 
+from . import views
+
+urlpatterns=[
+    path('', views.index, name='index'), 
+    path('login',views.login, name='login'),
+    path('register',views.register , name='register'),
+    path('write',views.write1,name='write'),
+    path('blogs',views.blogs ,name='blogs'),
+    path('post/<str:pk>',views.post,name='post')
+]
